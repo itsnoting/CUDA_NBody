b8bda5998c64aef091aa1ab11e148193f72f04ec
kting <kting93@gmail.com>
2016-05-16 16:58:16 -0700

    cleaning code

--- NBody/Nbody.cu
+++ NBody/Nbody.cu
@@ -19,6 +19,10 @@ __device__ float distance(float x1, float y1, float x2, float y2)
 	return sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
 }
 
+__global__ void print(){
+	return;
+}
+
 __global__ void initialize(float *pos_x, float* pos_y, float* masses, float* velocities_x, float* velocities_y){
 	curandState_t state;
 	curand_init(0, 0, 0, &state);
@@ -30,13 +34,16 @@ __global__ void initialize(float *pos_x, float* pos_y, float* masses, float* vel
 }
 __global__ void updateVelocities(float *masses, float* velocities_x, float* velocities_y, float* pos_x, float* pos_y)
 {
-	velocities_x[threadIdx.x] -= (G * masses[threadIdx.x] * masses[threadIdx.y]) / 
-		pow(distance(pos_x[threadIdx.x], pos_y[threadIdx.x], pos_x[threadIdx.y], pos_y[threadIdx.y]), 3) * 
-		(pos_x[threadIdx.x] - pos_x[threadIdx.y]);
-	velocities_y[threadIdx.x] -= (G * masses[threadIdx.x] * masses[threadIdx.y]) / 
-		pow(distance(pos_x[threadIdx.x], pos_y[threadIdx.x], pos_x[threadIdx.y], pos_y[threadIdx.y]), 3) * 
-		(pos_y[threadIdx.x] - pos_y[threadIdx.y]);
-
+	int q = threadIdx.x;
+	int k = threadIdx.y;
+	if (q != k){
+		velocities_x[q] -= (G * masses[q] * masses[k]) /
+			pow(distance(pos_x[q], pos_y[q], pos_x[k], pos_y[k]), 3) *
+			(pos_x[q] - pos_x[k]);
+		velocities_y[q] -= (G * masses[q] * masses[k]) /
+			pow(distance(pos_x[q], pos_y[q], pos_x[k], pos_y[k]), 3) *
+			(pos_y[q] - pos_y[k]);
+	}
 }
 __global__ void updatePositions(float *pos_x, float *pos_y, float* masses, float* velocities_x, float* velocities_y){
 	
@@ -55,30 +62,31 @@ int main() {
 	float h_distances_y[PARTICLE_COUNT] = {};
 
 	// Initialize the device variables
-	float d_pos_x[PARTICLE_COUNT] = {};
-	float d_pos_y[PARTICLE_COUNT] = {};
-	float d_masses[PARTICLE_COUNT] = {};
-	float d_velocities_x[PARTICLE_COUNT] = {};
-	float d_velocities_y[PARTICLE_COUNT] = {};
-	float d_distances_x[PARTICLE_COUNT] = {};
-	float d_distances_y[PARTICLE_COUNT] = {};
+	float d_pos_x[PARTICLE_COUNT];
+	float d_pos_y[PARTICLE_COUNT];
+	float d_masses[PARTICLE_COUNT];
+	float d_velocities_x[PARTICLE_COUNT];
+	float d_velocities_y[PARTICLE_COUNT];
+	float d_distances_x[PARTICLE_COUNT];
+	float d_distances_y[PARTICLE_COUNT];
 
 	if ( 
 		(cudaMalloc((void**)&d_pos_x, sizeof(float) * PARTICLE_COUNT) != cudaSuccess) ||
 		(cudaMalloc((void**)&d_pos_y, sizeof(float) * PARTICLE_COUNT) != cudaSuccess) ||
 		(cudaMalloc((void**)&d_masses, sizeof(float) * PARTICLE_COUNT) != cudaSuccess) ||
-		(cudaMalloc((void**)&d_velocities_x, sizeof(float) * PARTICLE_COUNT * DIM) != cudaSuccess) ||
-		(cudaMalloc((void**)&d_velocities_y, sizeof(float) * PARTICLE_COUNT * DIM) != cudaSuccess) ||
-		(cudaMalloc((void**)&d_distances_x, sizeof(float) * PARTICLE_COUNT * DIM) != cudaSuccess) ||
-		(cudaMalloc((void**)&d_distances_y, sizeof(float) * PARTICLE_COUNT * DIM) != cudaSuccess) )
+		(cudaMalloc((void**)&d_velocities_x, sizeof(float) * PARTICLE_COUNT) != cudaSuccess) ||
+		(cudaMalloc((void**)&d_velocities_y, sizeof(float) * PARTICLE_COUNT) != cudaSuccess) ||
+		(cudaMalloc((void**)&d_distances_x, sizeof(float) * PARTICLE_COUNT) != cudaSuccess) ||
+		(cudaMalloc((void**)&d_distances_y, sizeof(float) * PARTICLE_COUNT) != cudaSuccess) )
 
 	{
 		cout << "Error: Allocating memory" << endl;
 		return 1;
 	}
-	initialize << < PARTICLE_COUNT / 256 + 1, 256 >> >(d_pos_x, d_pos_y, d_masses, d_velocities_x, d_velocities_y);
+	//initialize<<<PARTICLE_COUNT / 256 + 1, 256>>>(d_pos_x, d_pos_y, d_masses, d_velocities_x, d_velocities_y);
 	for (int i = 0; i < TIMESTEPS; ++i){
-		updatePositions << <PARTICLE_COUNT / 256 + 1, 256 >> >(d_pos_x, d_pos_y, d_masses, d_velocities_x, d_velocities_y);
+		//updatePositions<<<PARTICLE_COUNT / 256 + 1, 256>>>(d_pos_x, d_pos_y, d_masses, d_velocities_x, d_velocities_y);
+		print <<<1, 5 >>>();
 		continue;
 	}
 
